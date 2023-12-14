1227a920f6b623740e2d235b0a2042ee70b5d8e3
Josephine Östman <josephine.een@gmail.com>
2023-12-14 16:28:26 +0100

    Added a few test for the return strings. May change

--- back-end/routes/routes_test.go
+++ back-end/routes/routes_test.go
@@ -8,13 +8,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPingRoute(t *testing.T) {
+func TestLivenessRoute(t *testing.T) {
 	router := SetupRouter()
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/liveness", nil)
+	req, _ := http.NewRequest("GET", "/", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "I live", w.Body.String())
+	assert.Equal(t, "Hello there!", w.Body.String())
+}
+func TestCompanyQueryString(t *testing.T) {
+	router := SetupRouter()
+
+	w := httptest.NewRecorder()
+	companyName := "Kanelbulle"
+	req, _ := http.NewRequest("GET", "/ticks?company="+companyName, nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "Company name provided "+companyName, w.Body.String())
 }
