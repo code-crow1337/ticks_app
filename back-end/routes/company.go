ed10540a85b7a5cf70965943fd365e9ce5415b95
Joséphine Östman <42445989+code-crow1337@users.noreply.github.com>
2023-12-17 23:38:28 +0100

    Missed that I need to query the DB fort both tables (#9)

--- back-end/routes/company.go
+++ back-end/routes/company.go
@@ -3,17 +3,18 @@ package routes
 import (
 	"database/sql"
 	"e-identitet/tick-api/utils"
+	"fmt"
 	"log"
 )
 
-func GetCompanies(db *sql.DB) []Company {
-	rows, err := db.Query("SELECT * FROM company;")
+func GetCompanies(db *sql.DB) []CompanyWithTick {
+	rows, err := db.Query("SELECT company.company_id, company.company_name, company.company_address, company.company_phone, company.company_mail, tick.tick_id, tick.core_service, tick.date, tick.type_tick FROM company INNER JOIN tick ON company.company_id = tick.company_id")
 	checkError := utils.CheckError
 	checkError(err)
-	companies := make([]Company, 0)
+	companies := make([]CompanyWithTick, 0)
 	for rows.Next() {
-		var company Company
-		if err := rows.Scan(&company.CompanyId, &company.CompanyName, &company.CompanyAddress, &company.CompanyMail, &company.CompanyPhone); err != nil {
+		var company CompanyWithTick
+		if err := rows.Scan(&company.CompanyId, &company.CompanyName, &company.CompanyAddress, &company.CompanyMail, &company.CompanyPhone, &company.TickId, &company.TypeTick, &company.CoreService, &company.Date); err != nil {
 			log.Fatal(err)
 		}
 
@@ -37,7 +38,22 @@ func GetCompanyNames(db *sql.DB) []string {
 	return names
 }
 
-func GetCompany(db *sql.DB) {
+func GetCompany(db *sql.DB, companyName string) []CompanyWithTick {
+	singleCompanyQuery := "SELECT company.company_id, company.company_name, company.company_address, company.company_phone, company.company_mail, tick.tick_id, tick.core_service, tick.date, tick.type_tick FROM company INNER JOIN tick ON company.company_id = tick.company_id WHERE company.company_name ILIKE " + fmt.Sprint("'"+companyName+"'")
+	log.Print("🧁", singleCompanyQuery)
+	rows, err := db.Query(singleCompanyQuery)
+	checkError := utils.CheckError
+	checkError(err)
+	companies := make([]CompanyWithTick, 0)
+	for rows.Next() {
+		var company CompanyWithTick
+		if err := rows.Scan(&company.Company.CompanyId, &company.CompanyName, &company.CompanyAddress, &company.CompanyMail, &company.CompanyPhone, &company.TickId, &company.TypeTick, &company.CoreService, &company.Date); err != nil {
+			log.Fatal(err)
+		}
+
+		companies = append(companies, company)
+	}
+	return companies
 }
 
 type Company struct {
@@ -46,4 +62,8 @@ type Company struct {
 	CompanyAddress string `json:"companyAddress"`
 	CompanyPhone   string `json:"companyPhone"`
 	CompanyMail    string `json:"companyMail"`
+}
+type CompanyWithTick struct {
+	Company
+	Tick
 }
